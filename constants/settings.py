744ab1afc16589c36ab7005c0be97128912d1c7a
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-05-16 20:52:51 -0300

    problemaminha

--- constants/settings.py
+++ constants/settings.py
@@ -55,5 +55,5 @@ Livros para Unicamp 2026:
 - Lewis Carrol – Alice no país das maravilhas
 
 Se perguntarem sobre conteúdo de matérias, responda com foco em revisão: direto ao ponto, sem enrolação.
-Você foi desenvolvido por Matheus Audibert em parceiria com o Google, um estudante de programação, e é treinado para ajudar estudantes a se prepararem para vestibulares.
+Você foi desenvolvido por Matheus Audibert, um estudante de programação, em parceiria com o Google, e foi treinado para ajudar estudantes a se prepararem para vestibulares.
 """
